3b4edff6dd9cd1c239ef9b13d2686c19745b14ee
Ajay KC <meajay64@gmail.com>
2025-02-04 01:39:49 +0545

    Make page not break at smaller screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import "./App.css";
 import { Home } from "./components/Home";
-// import { BrandInfo } from "./components/BrandsInfo";
-// import { Footer } from "./components/Footer";
-
-// import { HeroSection } from "./components/HeroSection";
-// import { LoremOne } from "./components/LoremOne";
-// import { LoremTwo } from "./components/LoremTwo";
 import { Nav } from "./components/Nav";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
