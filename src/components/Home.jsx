0deb435dfbc9ed74d79a2dc2b1405cacbe742964
Ajay KC <meajay64@gmail.com>
2025-02-04 01:16:11 +0545

    Remove all home page contents to its own component

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import { BrandInfo } from "./BrandsInfo";
+import { HeroSection } from "./HeroSection";
+import { LoremOne } from "./LoremOne";
+import { LoremTwo } from "./LoremTwo";
+
+export const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <BrandInfo />
+      <LoremOne />
+      <LoremTwo />
+    </>
+  );
+};
