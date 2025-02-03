60d947a2389e121d7f0ac61aa329517913246ce4
Ajay KC <meajay64@gmail.com>
2025-02-03 21:18:31 +0545

    Create, style and add Nav to App component

--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -1,3 +1,35 @@
+import "../../styles/Nav.css";
+import BrandLogo from "../images/brand-logo.png";
+import { PrimaryButton, SecondaryButton } from "./Button";
 export const Nav = () => {
-  return <p>Nav</p>;
+  return (
+    <nav>
+      <div className="nav-container fix-container">
+        <img src={BrandLogo} alt="logo" />
+
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/pricing">Pricing</a>
+          </li>
+          <li>
+            <a href="/service">Service</a>
+          </li>
+          <li>
+            <a href="/team">Team</a>
+          </li>
+        </ul>
+
+        <div className="btn-container">
+          <SecondaryButton btnText={"LOG IN"} url={"/login"} />
+          <PrimaryButton btnText={"GET STARTED"} url={"/"} />
+        </div>
+      </div>
+    </nav>
+  );
 };
